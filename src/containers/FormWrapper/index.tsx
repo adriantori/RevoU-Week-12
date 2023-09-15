26a3880ba7147270ebb489fdd5e55acdb2c4f3a7
Adri Antori <adriantori11@gmail.com>
2023-09-15 12:27:53 +0700

    feat: added step tracker on top and clickable tracker on the left

--- src/containers/FormWrapper/index.tsx
+++ src/containers/FormWrapper/index.tsx
@@ -1,40 +1,96 @@
 import { useState } from "react";
+import { Divider, Steps } from "antd"
 import { FormPersonal, FormAddress, FormAccount, FormFinish } from "..";
+import styles from './styles.module.css'
+
+const steps = [
+    {
+        title: 'Personal',
+        content: 'First-content',
+    },
+    {
+        title: 'Address',
+        content: 'Second-content',
+    },
+    {
+        title: 'Account',
+        content: 'Last-content',
+    }
+];
 
 const FormWrapper = () => {
     const [step, setStep] = useState<number>(1);
+    const [topStep, setTopStep] = useState<number>(0)
 
     const handleNext = () => {
-        if (step === 1 || step === 2 || step === 3) {
+        if (step < steps.length + 1) {
             setStep((prevStep) => prevStep + 1);
+            setTopStep((prevTopStep) => prevTopStep + 1);
         }
 
         return
     }
 
     const handlePrev = () => {
-        if (step === 2 || step === 3 || step === 4) {
+        if (step > 1) {
             setStep((prevStep) => prevStep - 1);
+            setTopStep((prevTopStep) => prevTopStep - 1);
         }
 
         return
     }
 
+    const onChange = (value: number) => {
+        if (value < topStep) {
+            setStep(value + 1);
+            setTopStep(value);
+        }
+    };
+
+
+    const items = steps.map((item) => ({ key: item.title, title: item.title }));
+
     return (
         <>
-            {step === 1 && (
-                <FormPersonal onNext={handleNext} />
-            )}
-            {step === 2 && (
-                <FormAddress onNext={handleNext} onPrev={handlePrev}/>
-            )}
-            {step === 3 && (
-                <FormAccount onNext={handleNext} onPrev={handlePrev}/>
-            )}
-            {step === 4 && (
-                <FormFinish onPrev={handlePrev}/>
-            )}
-
+            <Steps
+                current={topStep}
+                items={items}
+            />
+            <Divider />
+            <div className={styles.flexbox}>
+                <div>
+                    <Steps
+                        current={topStep}
+                        onChange={onChange}
+                        direction="vertical"
+                        items={[
+                            {
+                                
+                            },
+                            {
+                                
+                            },
+                            {
+                                
+                            },
+                        ]}
+                    />
+                </div>
+                <div>
+                    {step === 1 && (
+                        <FormPersonal onNext={handleNext} />
+                    )}
+                    {step === 2 && (
+                        <FormAddress onNext={handleNext} onPrev={handlePrev} />
+                    )}
+                    {step === 3 && (
+                        <FormAccount onNext={handleNext} onPrev={handlePrev} />
+                    )}
+                    {step === 4 && (
+                        <FormFinish onPrev={handlePrev} />
+                    )}
+                </div>
+            </div>
         </>
     )
 }
